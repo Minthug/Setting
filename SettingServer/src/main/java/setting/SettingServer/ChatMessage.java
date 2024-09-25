50783cebda552910ad9befc0585f78344c6f3ae0
minthug <jkisimmortal4@gmail.com>
2024-09-25 16:39:13 +0900

    Add WebSocket support for chat functionality

---
+++ SettingServer/src/main/java/setting/SettingServer/ChatMessage.java
@@ -0,0 +1,18 @@
+package setting.SettingServer;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+}
