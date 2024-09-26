66973642f6c67bdfe97a175f65b0082d1f55bd2c
minthug <jkisimmortal4@gmail.com>
2024-09-26 16:31:21 +0900

    Add Redis support and configuration

---
+++ SettingServer/src/main/java/setting/SettingServer/service/RedisService.java
@@ -0,0 +1,30 @@
+package setting.SettingServer.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class RedisService {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void save(String key, String value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public void setValue(String key, String value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public String getValue(String key) {
+        return (String) redisTemplate.opsForValue().get(key);
+    }
+
+    public void deleteValue(String key) {
+        redisTemplate.delete(key);
+    }
+}
---
+++ SettingServer/src/test/java/setting/SettingServer/service/RedisServiceTest.java
@@ -0,0 +1,26 @@
+package setting.SettingServer.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class RedisServiceTest {
+
+    @Autowired
+    private RedisService redisService;
+
+    @Test
+    public void testRedisOperations() {
+        String key = "testKey";
+        String value = "testValue";
+
+        redisService.setValue(key, value);
+        assertEquals(value, redisService.getValue(key));
+
+        redisService.deleteValue(key);
+        assertNull(redisService.getValue(key));
+    }
+}
