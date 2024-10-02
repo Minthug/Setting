834f17855e3d176f1c0af4102892a9b55a79b760
minthug <jkisimmortal4@gmail.com>
2024-10-02 17:36:07 +0900

    Add OAuth2 login and CORS configurations

--- SettingServer/src/main/java/setting/SettingServer/common/oauth/handler/OauthLoginFailureHandler.java
+++ SettingServer/src/main/java/setting/SettingServer/common/oauth/handler/OauthLoginFailureHandler.java
@@ -1,4 +1,25 @@
 package setting.SettingServer.common.oauth.handler;
 
-public class OauthLoginFailureHandler {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OauthLoginFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.getWriter().write("OAuth2 로그인 실패, 서버 로그 확인 해주세요");
+        log.info("OAuth2 소셜 로그인 실패, error msg: {}", exception.getMessage());
+    }
 }
--- SettingServer/src/main/java/setting/SettingServer/config/SecurityConfig.java
+++ SettingServer/src/main/java/setting/SettingServer/config/SecurityConfig.java
@@ -1,14 +1,39 @@
 package setting.SettingServer.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import setting.SettingServer.common.oauth.CustomOauthUserService;
 import setting.SettingServer.common.oauth.handler.OauthLoginFailureHandler;
 import setting.SettingServer.common.oauth.handler.OauthLoginSuccessHandler;
+import setting.SettingServer.config.jwt.filter.CustomJsonUsernamePasswordAuthenticationFilter;
+import setting.SettingServer.config.jwt.filter.JwtAuthenticationProcessingFilter;
+import setting.SettingServer.config.jwt.handler.LoginFailureHandler;
+import setting.SettingServer.config.jwt.handler.LoginSuccessHandler;
 import setting.SettingServer.config.jwt.service.JwtService;
 import setting.SettingServer.config.jwt.service.LoginService;
 import setting.SettingServer.repository.UserRepository;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,5 +46,106 @@ public class SecurityConfig {
     private final LoginService loginService;
     private final OauthLoginSuccessHandler oauthLoginSuccessHandler;
     private final OauthLoginFailureHandler oauthLoginFailureHandler;
+    private final CustomOauthUserService customOauthUserService;
 
+    private static final String[] PUBLIC_URLS = {
+            "/v1/auth/**", "/v1/upload/**", "/v1/boards","/oauth2/authorization/**", "/",
+            "/css/**", "/images/**", "/js/**", "/h2-console/**", "/favicon.ico", "/error", "/v1/oauth/**"
+        };
+
+    private static final String[] USER_ADMIN_AUTHORITIES = {"USER", "ADMIN"};
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.configurationSource(configurationSource()))
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable())
+                .headers(headers -> headers.frameOptions().disable())
+                .authorizeHttpRequests(auth -> configureAuthorizeHttpRequests(auth))
+                .oauth2Login(oauth2 -> configureOauth2Login(oauth2))
+                .addFilterAfter(customJsonUsernamePasswordAuthenticationFilter(), LogoutFilter.class)
+                .addFilterBefore(jwtAuthenticationProcessingFilter(), CustomJsonUsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter() {
+        JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter = new JwtAuthenticationProcessingFilter(jwtService, userRepository);
+        return jwtAuthenticationProcessingFilter;
+    }
+
+    @Bean
+    public CustomJsonUsernamePasswordAuthenticationFilter customJsonUsernamePasswordAuthenticationFilter() {
+        CustomJsonUsernamePasswordAuthenticationFilter customJsonUsernamePasswordAuthenticationFilter =
+                new CustomJsonUsernamePasswordAuthenticationFilter(objectMapper);
+        customJsonUsernamePasswordAuthenticationFilter.setAuthenticationManager(authenticationManager());
+        customJsonUsernamePasswordAuthenticationFilter.setAuthenticationSuccessHandler(loginSuccessHandler());
+        customJsonUsernamePasswordAuthenticationFilter.setAuthenticationFailureHandler(loginFailureHandler());
+        return customJsonUsernamePasswordAuthenticationFilter;
+    }
+
+    private void configureOauth2Login(OAuth2LoginConfigurer<HttpSecurity> oauth2) {
+        oauth2
+                .loginPage("/v1/oauth/oauth2/authorization/{provider}")
+                .successHandler(oauthLoginSuccessHandler)
+                .failureHandler(oauthLoginFailureHandler)
+                .userInfoEndpoint(userInfo -> userInfo.userService(customOauthUserService));
+    }
+
+    private void configureAuthorizeHttpRequests(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry auth) {
+        auth
+                .requestMatchers(PUBLIC_URLS).permitAll()
+                .requestMatchers(HttpMethod.GET, "/v1/users", "/v1/users/{id}").permitAll()
+                .requestMatchers(HttpMethod.PATCH, "/v1/users/{id}").hasAnyAuthority(USER_ADMIN_AUTHORITIES);
+
+        configureUserAdminRequests(auth);
+        auth.anyRequest().authenticated();
+    }
+
+    private void configureUserAdminRequests(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry auth) {
+        auth
+                .requestMatchers(HttpMethod.GET, "/v1/users/profile").hasAnyAuthority(USER_ADMIN_AUTHORITIES)
+                .requestMatchers(HttpMethod.DELETE, "/v1/users/**").hasAnyAuthority(USER_ADMIN_AUTHORITIES);
+    }
+
+    @Bean
+    public CorsConfigurationSource configurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        configuration.setExposedHeaders(Arrays.asList("Authorization", "RefreshToken"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public LoginFailureHandler loginFailureHandler() {
+        return new LoginFailureHandler();
+    }
+
+    @Bean
+    public LoginSuccessHandler loginSuccessHandler() {
+        return new LoginSuccessHandler(jwtService, userRepository);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(loginService);
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }
